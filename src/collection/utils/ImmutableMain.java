be0dc731484e5ea3e690b063d838e3d9e4cef7cf
hyun <apfhd0257@naver.com>
2025-04-30 18:57:32 +0900

    Collections 유틸

---
+++ src/collection/utils/ImmutableMain.java
@@ -0,0 +1,23 @@
+package collection.utils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ImmutableMain {
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1, 2, 3);
+
+        // 불변 -> 가변
+        ArrayList<Integer> mutableList = new ArrayList<>(list);
+        mutableList.add(4);
+        System.out.println("mutableList = " + mutableList);
+        System.out.println("mutableList.getClass() = " + mutableList.getClass());
+
+        // 가변 -> 불변
+        List<Integer> unmodifiableList = Collections.unmodifiableList(mutableList);
+        System.out.println("unmodifiableList.getClass() = " + unmodifiableList.getClass());
+        // 불변이므로 변경 불가
+//        unmodifiableList.add(5);
+    }
+}
